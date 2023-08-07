46ef7d166d3bcc7ab4d39585c4a737f7174899fc
Laszlo Gecse <lgecse@cisco.com>
2023-08-07 18:29:15 +0200

    Formatting only.
    Signed-off-by: Laszlo Gecse <lgecse@cisco.com>

--- internal/options/options.go
+++ internal/options/options.go
@@ -23,21 +23,20 @@ import (
 )
 
 type Options struct {
-	LogLevel     string
-	ConfigFile   string
-	GitHubToken  string
-	JiraUser     string
-	JiraPassword string
-	RepoName     string
-	JiraURI      string
-	JiraProject  string
+	LogLevel       string
+	ConfigFile     string
+	GitHubToken    string
+	JiraUser       string
+	JiraPassword   string
+	RepoName       string
+	JiraURI        string
+	JiraProject    string
+	JiraComponents string
 	// TODO(options): Should this be a time type?
 	Since   string
 	Confirm bool
 	Timeout time.Duration
 	Period  time.Duration
-
-	JiraComponents string
 }
 
 const (
